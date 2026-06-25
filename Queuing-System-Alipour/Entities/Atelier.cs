ee84d44ff6912649646b47f1945e6e7e7b894a89
Sobhan Rajabi <sobhanrajabi31@gmail.com>
2026-06-25 13:06:36 +0330

    Add Database Entities.

---
+++ Queuing-System-Alipour/Entities/Atelier.cs
@@ -0,0 +1,16 @@
+﻿namespace Queuing_System_Alipour.Entities
+{
+    public sealed class Atelier
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime QueueCreatedAt { get; set; }
+        public TimeSpan QueueDuration { get; set; }
+        public int QueueStatus { get; set; }
+        public string Note { get; set; }
+
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
