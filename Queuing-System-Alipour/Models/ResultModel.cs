219ad275a8cfed527c96248c794c23721ef6bdd4
Sobhan Rajabi <sobhanrajabi31@gmail.com>
2026-06-25 13:08:08 +0330

    Modify Models directory.

---
+++ Queuing-System-Alipour/Models/ResultModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Queuing_System_Alipour.Models
+{
+    public class ResultModel
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ResultModel<T> : ResultModel
+    {
+        public T Data { get; set; }
+    }
+}
--- Queuing-System-Alipour/Models/UsersModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Queuing_System_Alipour.Models
-{
-    public class UsersModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
