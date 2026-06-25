b09131f485e1970942a90e01b84bd2417e69bf94
Sobhan Rajabi <sobhanrajabi31@gmail.com>
2026-06-25 13:07:06 +0330

    Add Database DataContext.

---
+++ Queuing-System-Alipour/AppDataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Queuing_System_Alipour.Configurations;
+using Queuing_System_Alipour.Entities;
+using System.Configuration;
+
+namespace Queuing_System_Alipour
+{
+    public sealed class AppDataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Connections.AppCS);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EmployeeConfig());
+            modelBuilder.ApplyConfiguration(new AtelierConfig());
+            modelBuilder.ApplyConfiguration(new PersonnelConfig());
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Atelier> Ateliers { get; set; }
+        public DbSet<Personnel> Personnels { get; set; }
+    }
+}
---
+++ Queuing-System-Alipour/Connections.cs
@@ -0,0 +1,7 @@
+﻿namespace Queuing_System_Alipour
+{
+    public static class Connections
+    {
+        public static string AppCS => Environment.GetEnvironmentVariable("QueuingSystemCS", EnvironmentVariableTarget.User);
+    }
+}
