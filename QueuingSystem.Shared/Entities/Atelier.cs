7cfec2ddccf2effe6bd57ef855cf910d25458ae7
Sobhan Rajabi <sobhanrajabi31@gmail.com>
2026-06-28 02:14:32 +0330

    Add QueuingSystem.Shared.Entities.

---
+++ QueuingSystem.Shared/Entities/Atelier.cs
@@ -0,0 +1,23 @@
+﻿namespace QueuingSystem.Shared.Entities
+{
+    public enum QueueStatus
+    {
+        Pending,
+        Done,
+        Canceled
+    }
+
+    public sealed class Atelier
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime QueueCreatedAt { get; set; }
+        public DateTime QueueEndAt { get; set; }
+        public QueueStatus QueueStatus { get; set; }
+        public string? Note { get; set; }
+
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
---
+++ QueuingSystem.Shared/Entities/Personnel.cs
@@ -0,0 +1,14 @@
+﻿namespace QueuingSystem.Shared.Entities
+{
+    public sealed class Personnel
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public DateTime QueueCreatedAt { get; set; }
+        public DateTime QueueEndedAt { get; set; }
+        public bool QueueStatus { get; set; }
+
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
