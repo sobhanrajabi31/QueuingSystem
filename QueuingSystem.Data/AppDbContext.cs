447af08cc048b32100c3a96689e96f069fde4e9b
Sobhan Rajabi <sobhanrajabi31@gmail.com>
2026-06-28 02:11:43 +0330

    Add QueuingSystem.Data project.

---
+++ QueuingSystem.Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QueuingSystem.Data.Configurations;
+using QueuingSystem.Shared.Entities;
+
+namespace QueuingSystem.Data
+{
+    public sealed class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Connections.AppCS);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EmployeeConfig());
+            modelBuilder.ApplyConfiguration(new AtelierConfig());
+            modelBuilder.ApplyConfiguration(new PersonnelConfig());
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Atelier> Ateliers { get; set; }
+        public DbSet<Personnel> Personnels { get; set; }
+    }
+}
---
+++ QueuingSystem.Data/Connections.cs
@@ -0,0 +1,7 @@
+﻿namespace QueuingSystem.Data
+{
+    public static class Connections
+    {
+        public static string AppCS => Environment.GetEnvironmentVariable("QueuingSystemCS", EnvironmentVariableTarget.User);
+    }
+}
