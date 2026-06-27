cd4ff04a6973ebf69740ac4c945d938f5a3f1a8e
Sobhan Rajabi <sobhanrajabi31@gmail.com>
2026-06-28 02:10:16 +0330

    Add QueuingSystem.Client.Tool.

---
+++ QueuingSystem.Client/Tool/Img.cs
@@ -0,0 +1,12 @@
+﻿namespace QueuingSystem.Client.Tool
+{
+    public static class Img
+    {
+        public static Bitmap ConvertToBmp(Image img)
+        {
+            var bmp = new Bitmap(img);
+            bmp.MakeTransparent(Color.White);
+            return bmp;
+        }
+    }
+}
---
+++ QueuingSystem.Client/Tool/SafeLabel.cs
@@ -0,0 +1,12 @@
+﻿namespace QueuingSystem.Client.Tool
+{
+    public sealed class SafeLabel : Label
+    {
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x203)
+                return;
+            base.WndProc(ref m); 
+        }
+    }
+}
